201654a6a3e696ddd560f21bdca00dd82ad88180
katiiiasal <2123100501@soy.utj.edu.mx>
2024-03-08 14:24:15 -0600

    Proyecto Creado

---
+++ src/main/java/com/mycompany/qualitas/Qualitas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.qualitas;
+
+/**
+ *
+ * @author heiva
+ */
+public class Qualitas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
